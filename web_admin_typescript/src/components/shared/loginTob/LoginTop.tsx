6c15e34bfa474448326a000c138d76b2bbdeef0f
Tuhin810 <tuhin.thakur1233@gmail.com>
2024-05-14 21:07:08 +0530

    FIX: login screen title fixed

--- web_admin_typescript/src/components/shared/loginTob/LoginTop.tsx
+++ web_admin_typescript/src/components/shared/loginTob/LoginTop.tsx
@@ -7,7 +7,7 @@ const LoginTop = () => {
        <img className='h-12 w-12 mx-auto mb-1' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8HgMZGCngrcuMN_Wks--OBJlchjSEIhRF6twjYXYkhw&s" alt="" />
        <div className="text-sm font-semibold text-green-800">West Bengal Pollution Control Board</div>
     </div>
-    <div className="text-2xl my-3 font-bold text-gray-800">Cloth Bag Vending Machine</div>
+    <div className="text-2xl my-3 font-bold text-gray-800">Cloth Bag Vending Machine Dashboard</div>
     </div>
   )
 }
