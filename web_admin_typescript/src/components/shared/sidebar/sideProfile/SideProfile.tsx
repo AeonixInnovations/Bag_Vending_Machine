e0adb2ab37ff673a1eb02c4e59c28149252ab6a3
pritam_majhi <pritammajhi10@gmail.com>
2024-06-07 15:29:18 +0530

    current push

--- web_admin_typescript/src/components/shared/sidebar/sideProfile/SideProfile.tsx
+++ web_admin_typescript/src/components/shared/sidebar/sideProfile/SideProfile.tsx
@@ -9,15 +9,16 @@ const SideProfile = () => {
     <div>
       <div className="flex flex-col items-center mt-6 -mx-2">
         <div className="w-24 h-24 mx-2 flex items-center justify-center rounded-full bg-green-400">
-          <h4 className="text-3xl text-gray-100 font-semibold">AF</h4>
+          <h4 className="text-3xl text-gray-100 font-semibold">A</h4>
         </div>
         <h4 className="mx-2 mt-2 font-medium text-gray-200">
-          {user?.user_name || "John Bhai"}
+          {/* {user?.user_name || "John Bhai"} */}
+          Admin
         </h4>
-        <p className="mx-2 mt-1 text-sm font-medium text-gray-400">
-          {/* john@example.com */}
+        {/* <p className="mx-2 mt-1 text-sm font-medium text-gray-400">
+          
           {user?.email || "John Bhai"}
-        </p>
+        </p> */}
       </div>
     </div>
   );
